964b90b7b83cb08d62588c823f9f9b468cc30a49
Avishka Niroshana <avishkanperera4@gmail.com>
2025-08-28 00:11:19 +0530

    Setup the project with tailwind css and implemented chat and main page in frontend

--- chatapp-frontend/src/main.jsx
+++ chatapp-frontend/src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter } from 'react-router'
-import AppRoutes from './config/Routes.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./config/Routes.jsx";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <AppRoutes/>
+      <Toaster position="top-right" />
+      <AppRoutes />
     </BrowserRouter>
   </StrictMode>
 );
